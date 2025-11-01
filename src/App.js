547a0a2bcd8e848a9090f16820263c5a36b5e883
Aarohi <aarohi.6364@gmail.com>
2025-11-02 02:08:57 +0530

    List of books Persisted with outside API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    // DONT DO THIS:
+    // fetchBooks();
+
+    useEffect(() => {
+        fetchBooks();
+    }, [])
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`,{
+            title: newTitle,
+        });
+
+        // console.log(response);
+
         const updatedBooks = books.map((book) => {
             if(book.id === id){
                 return {
                     ...book,
-                    title: newTitle
+                    ...response.data
                 }
             }
             return book;
@@ -19,7 +39,9 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
@@ -27,25 +49,20 @@ function App() {
         setBooks(updatedBooks);
     };
 
-    const createBook = (title) => {
-        // console.log('Need to add book with: ',title);
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
 
-        // BAD CODE!
-        // books.push({id: 123, title: title});
-        // console.log(books);
-        // setBooks(books);
-
+        // console.log(response);
 
         const updatedBooks = [
             ...books,
-            {
-                id: Math.round(Math.random() * 9999),
-                title
-            }
+            response.data
         ];
 
         setBooks(updatedBooks);
-    };
+    }; 
 
     return <div className="app">
         <h1>Reading List</h1>
